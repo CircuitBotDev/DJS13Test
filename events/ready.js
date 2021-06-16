29836bf150191d9b928ed356c36a81305058e0ae
JAGUARAVI <81582444+AviDoesDev@users.noreply.github.com>
2021-06-16 10:47:09 +0530

    Revert "Okay"
    This reverts commit ba8af01efa513d9ef907d83bbfc4077b04ef7048.

---
+++ events/ready.js
@@ -0,0 +1,20 @@
+const BaseEvent = require('../classes/base/BaseEvent.js');
+
+module.exports = class Event extends BaseEvent {
+    constructor() {
+        super('ready');
+    }
+
+    async run(client) {
+        /*await client.utils.wait(1000);
+        await Promise.all(
+            client.guilds.cache.map(async guild => {
+                await guild.members.fetch();
+                await guild.fetchInvites().then(async invites => {
+                    client.db.invites.set(guild.id,invites);
+                }).catch(() => {});
+        }));*/
+
+        client.log.success(`${client.user.tag} ready!`);
+    }
+}
