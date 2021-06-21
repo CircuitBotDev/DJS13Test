b0aa059a33fcdf50823d169be1bfbb232496f63f
JAGUARAVI <81582444+AviDoesDev@users.noreply.github.com>
2021-06-21 08:43:21 +0530

    Update General.js

--- utils/General.js
+++ utils/General.js
@@ -99,5 +99,26 @@ module.exports = {
         } catch (e) {
             return false;
         }
+    },
+
+    removeDuplicates(arr) {
+        let n = arr.length;
+        
+        if (n == 0 || n == 1)
+            return n;
+
+        let temp = new Array(n);
+
+        let j = 0;
+        for (let i = 0; i < n - 1; i++)
+            if (arr[i] != arr[i + 1])
+                temp[j++] = arr[i];
+
+        temp[j++] = arr[n - 1];
+
+        for (let i = 0; i < j; i++)
+            arr[i] = temp[i];
+
+        return j;
     }
 }
